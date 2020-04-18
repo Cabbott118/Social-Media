611fcf24406442914c6bb3b84d24b018fe9f49ac
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-18 13:21:54 -0400

    First commit. Create-react-app.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+console.log(`App spinning up...`);
